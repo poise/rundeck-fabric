6e203c411886c7e41ba2aefe66300430dce53d6f
Noah Kantrowitz <noah@coderanger.net>
2014-04-24 17:57:52 -0700

    tests \o/

--- test/integration/simple/serverspec/simple_spec.rb
+++ test/integration/simple/serverspec/simple_spec.rb
@@ -19,3 +19,34 @@
 require 'serverspec'
 include Serverspec::Helper::Exec
 include Serverspec::Helper::DetectOS
+
+ENV['RDECK_BASE'] = '/var/lib/rundeck'
+
+describe command('rd-jobs -p fabric --name one --file /tmp/one --format yaml') do
+  it { should return_exit_status(0) }
+end
+
+describe file('/tmp/one') do
+  it { should be_a_file }
+  its(:content) { should include('description: Task one.') }
+end
+
+describe command('rd-jobs -p fabric --name two --file /tmp/two --format yaml') do
+  it { should return_exit_status(0) }
+end
+
+describe file('/tmp/two') do
+  it { should be_a_file }
+  its(:content) { should include("description: |-\n    Task\n        two.") }
+  its(:content) { should include("options:\n    arg1:\n      required: true") }
+end
+
+describe command('rd-jobs -p fabric --name three --file /tmp/three --format yaml') do
+  it { should return_exit_status(0) }
+end
+
+describe file('/tmp/three') do
+  it { should be_a_file }
+  its(:content) { should include('description: Take three.') }
+  its(:content) { should include("options:\n    c:\n      required: true\n    d:\n      value: '1'") }
+end
